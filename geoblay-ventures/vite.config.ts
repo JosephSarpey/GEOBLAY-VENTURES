e325000dc400f243b814a296b5fb122ad83057b4
JosephSarpey <josephsarpey2002@gmail.com>
2025-08-14 01:48:12 +0530

    added vercel.json for test deployment

--- geoblay-ventures/vite.config.ts
+++ geoblay-ventures/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [
+    react(),
     tailwindcss(),
   ],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
 })
